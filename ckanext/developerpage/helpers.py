83e3650bd92563fe77e2c26038947aeb7b8085ff
Sivan Grünberg <sivan.grunberg@datopian.com>
2019-07-06 02:32:53 +0300

    - fix system information dictionary creation - tap into `status_show` API call to get list of installed plugins and some other bits of CKAN config

--- ckanext/developerpage/helpers.py
+++ ckanext/developerpage/helpers.py
@@ -6,6 +6,13 @@ import os
 
 from ckan.common import config, is_flask_request, c, request
 from ckan.plugins import toolkit
+from ckan.logic.action.get import status_show
+
+import ckan.model as model
+import ckan.logic as logic
+
+from ckan.common import g, config, _
+
 
 
 import platform
@@ -13,24 +20,29 @@ import platform
 log = logging.getLogger(__name__)
 
 
-
 def memory_info():
     import psutil
     return dict(psutil.virtual_memory()._asdict())
 
+def average_load():
+    import psutil
+    return psutil.getloadavg()
+
 def get_host_info():
     python_platform = {
-            'machine' : platform.machine(),
-            'dist' : platform.dist(),
-            'py_build' : platform.python_build(),
-            'libc' : platform.libc_ver(),
+            'machine_architecture' : platform.machine(),
+            'python_build' : platform.python_build(),
         }
     memory = memory_info()
+    load = average_load()
     python_platform.update(memory)
+    python_platform.update({'load_average' : load})
     return python_platform
 
 def get_ckan_info():
-    log.info(config)
-    log.info(c)
-    return { 'info_item' : 'info_value' }
+    context = {
+            u'model': model,
+            u'user': g.user,
+            u'auth_user_obj': g.userobj}
+    return status_show(context, data_dict={})
 
